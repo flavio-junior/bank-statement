4ffe09b831d25eac0d1b20cde8978d2767d93981
flavio-junior <flaviojunior.work@gmail.com>
2024-05-28 20:21:32 -0300

    init project

---
+++ despesas-pessoais-back-end/src/main/kotlin/br/com/despesas/pessoais/DespesasPessoaisApplication.kt
@@ -0,0 +1,11 @@
+package br.com.despesas.pessoais
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DespesasPessoaisApplication
+
+fun main(args: Array<String>) {
+	runApplication<DespesasPessoaisApplication>(*args)
+}
---
+++ despesas-pessoais-back-end/src/test/kotlin/br/com/despesas/pessoais/DespesasPessoaisApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.despesas.pessoais
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DespesasPessoaisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
